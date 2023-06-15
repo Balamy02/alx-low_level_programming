22e37344bf85a997c9f38308b721509793a87987
Balamy02 <ahmedbalamypro@gmail.com>
2023-06-15 15:54:34 -0700

    C ppaa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints a specific sentence to the standard error
+ * Description: Prints a specific sentence to the standard error.
  *
  * Return: Always 1 (Failure)
  */
@@ -11,9 +11,10 @@ int main(void)
 {
 	ssize_t len;
 	int stderr_fd = 2;
+	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-	write(stderr_fd, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+	write(stderr_fd, str, len);
 
 	return (1);
 }
