0b13c86385b8aafb0a1aa593a2baf8be2f01d7f8
Balamy02 <ahmedbalamypro@gmail.com>
2023-06-15 16:06:17 -0700

    C ppaa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,19 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: Prints a quote using the write function.
- *
  * Return: Always 1 (Error)
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(quote) - 1;
-	ssize_t bytes_written = write(2, quote, len);
+    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 59;
 
-	if (bytes_written != len)
-		return (1);
-
-	return (1);
+    write(2, quote, len);
+    return (1);
 }
 
