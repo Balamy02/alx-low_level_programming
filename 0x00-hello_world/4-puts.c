19ef452ea1fc1c8e220c3d1908a004dee5f85388
Balamy02 <ahmedbalamypro@gmail.com>
2023-06-15 16:08:34 -0700

    C ppaa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: A C program that prints using the puts function.
- *
- * Return: Always 0
- */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
+    puts("Programming is like building a multilingual puzzle");
     return 0;
 }
 
