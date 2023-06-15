1362ba4daaf26d1102c8bf4d1e4c8c05f90aab80
Balamy02 <ahmedbalamypro@gmail.com>
2023-06-15 15:43:19 -0700

    C ppppa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-int main() { return printf("with proper grammar, but the outcome is a piece of art,\n") >= 0 ? 0 : 1; }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a piece of art using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
