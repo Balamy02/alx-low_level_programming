b722f61726073f8d8c8a6d8e2823478ca0013ddb
Balamy02 <ahmedbalamypro@gmail.com>
2023-06-15 15:44:15 -0700

    C ppppa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,23 @@
-#!/bin/bash
-int main() { write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57); return 1; }
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a specific sentence to the standard error
+ *
+ * Return: Always 1 (Failure)
+ */
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = strlen(message);
+
+	if (write(2, message, len) != len)
+		exit(1);
+
+	return 1;
+}
+
