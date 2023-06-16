4c659c5af95f594e791f5537929494b5868497e0
Balamy02 <ahmedbalamypro@gmail.com>
2023-06-16 14:17:05 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 int main() {
     int n;
 
-    srand(time(0)); // Initialize random seed
+    srand(time(0)); /* Initialize random seed */
 
-    n = rand() % 201 - 100; // Generate a random number between -100 and 100
+    n = rand() % 201 - 100; /* Generate a random number between -100 and 100 */
 
     printf("The number %d is ", n);
 
