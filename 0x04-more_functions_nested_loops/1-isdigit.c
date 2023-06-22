28961c0cc063ad00299784900c69d18250c62cf8
Balamy02 <ahmedbalamypro@gmail.com>
2023-06-22 14:54:00 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,47 +1,14 @@
-// main.h
-#ifndef MAIN_H
-#define MAIN_H
-
 int _isdigit(int c);
 
-#endif /* MAIN_H */
-
-
-// 1-isdigit.c
-#include "main.h"
-
 /**
- * _isdigit - check if a character is a digit (0-9)
- * @c: character to be checked
- *
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - check if a character is a digit
+ * @x: The number to be checked
+ * Return: 1 for a digit or 0 for any else
  */
-int _isdigit(int c)
+int _isdigit(int x)
 {
-    if (c >= '0' && c <= '9')
+    if (x >= 48 && x <= 57)
         return (1);
     else
         return (0);
 }
-
-
-// 1-main.c
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
-}
-
