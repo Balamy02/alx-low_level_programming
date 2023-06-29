b667d82f44ff6e265f84ba64cdfa315dc2d2cddf
Balamy02 <ahmedbalamypro@gmail.com>
2023-06-29 14:35:46 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,6 +5,7 @@ int main(void)
   int n;
   int a[5];
   int *p;
+  int *q; // Nouveau pointeur
 
   a[2] = 1024;
   p = &n;
@@ -16,7 +17,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+  q = p + 2; // Assigner q pour pointer vers la même position que p + 2
+  *q = 98;  // Modifier la valeur à cette position à l'aide de q
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
